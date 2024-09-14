d967fd2d6ca5b755eafe522986b75a11aa4cc0d1
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-09-14 14:03:58 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
-basic.forever(function () {
-	
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    btf.fill_sendBuffer19()
+    sender.send20Strecken(
+    btf.btf_sendBuffer19(),
+    sender.sender_StreckePicker(50, 180, 30),
+    sender.sender_StreckePicker(-50, 180, 30),
+    sender.sender_StreckePicker(100, 90, 20),
+    sender.sender_StreckePicker(50, 0, 115),
+    sender.sender_StreckePicker(-100, 90, 20),
+    1
+    )
+    btf.sendData(btf.btf_sendBuffer19())
+    btf.zeige5x5Buffer(btf.btf_sendBuffer19())
+    btf.zeige5x5Joystick(btf.btf_sendBuffer19())
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    btf.fill_sendBuffer19()
+    sender.send2x2Motoren(
+    btf.btf_sendBuffer19(),
+    sender.sender_2MotorenZeitPicker(80, 80, sender.sender_zehntelsekunden(btf.ePause.s2), 1, false),
+    sender.sender_2MotorenEncoder(btf.speedPicker(25), btf.speedPicker(-25), 198, 198, 1, true),
+    4
+    )
+    btf.sendData(btf.btf_sendBuffer19())
+    btf.zeige5x5Buffer(btf.btf_sendBuffer19())
+    btf.zeige5x5Joystick(btf.btf_sendBuffer19())
+})
+input.onButtonEvent(Button.B, btf.buttonEventValue(ButtonEvent.Hold), function () {
+    btf.buttonBhold()
+})
+input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
+    btf.buttonAhold()
+})
+sender.beimStart()
+basic.showNumber(btf.speedPicker(88))
