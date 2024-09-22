c68a7ddf1777fce3ce40a00366a26a9c783f6e1e
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-09-22 21:11:23 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,21 +4,33 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     btf.sendData(btf.btf_sendBuffer19())
     sender.send20Strecken(
     btf.btf_sendBuffer19(),
-    sender.sender_StreckePicker(50, 180, 30, false),
-    sender.sender_StreckePicker(-50, 180, 30, false),
-    sender.sender_StreckePicker(100, 90, 20, false),
-    sender.sender_StreckePicker(50, 0, 115, false),
-    sender.sender_StreckePicker(-100, 90, 20, false),
+    sender.sender_StreckePicker(50, 180, 30),
+    sender.sender_StreckePicker(-50, 180, 30),
+    sender.sender_StreckePicker(100, 90, 20),
+    sender.sender_StreckePicker(50, 0, 115),
+    sender.sender_StreckePicker(-100, 90, 20),
     1
     )
-    btf.setAbstand(btf.btf_sendBuffer19(), btf.e3Abstand.u0)
+    btf.setAbstand(btf.btf_sendBuffer19(), btf.e3Abstand.u2)
     btf.setaktiviert(btf.btf_sendBuffer19(), btf.e3aktiviert.m1, true)
     btf.sendData(btf.btf_sendBuffer19())
     btf.zeige5x5Buffer(btf.btf_sendBuffer19())
     btf.zeige5x5Joystick(btf.btf_sendBuffer19())
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-	
+    btf.fill_sendBuffer19()
+    btf.setBetriebsart(btf.btf_sendBuffer19(), btf.e0Betriebsart.p2Fahrplan)
+    btf.sendData(btf.btf_sendBuffer19())
+    sender.send2x2Motoren(
+    btf.btf_sendBuffer19(),
+    sender.sender_2MotorenPicker(80, 80, 25),
+    sender.sender_2Motoren(btf.speedPicker(25), btf.speedPicker(-25), 198, 198, false, true),
+    4
+    )
+    btf.setaktiviert(btf.btf_sendBuffer19(), btf.e3aktiviert.ma, true)
+    btf.sendData(btf.btf_sendBuffer19())
+    btf.zeige5x5Buffer(btf.btf_sendBuffer19())
+    btf.zeige5x5Joystick(btf.btf_sendBuffer19())
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     btf.fill_sendBuffer19()
@@ -26,9 +38,9 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     btf.sendData(btf.btf_sendBuffer19())
     sender.send2x2Motoren(
     btf.btf_sendBuffer19(),
-    sender.sender_2MotorenZeitPicker(80, 80, 20, 1, false),
-    sender.sender_2MotorenEncoder(btf.speedPicker(25), btf.speedPicker(-25), 198, 198, 1, true),
-    4
+    sender.sender_2MotorenPicker(80, 80, 25),
+    sender.sender_2MotorenPicker(50, -50, 25),
+    1
     )
     btf.setaktiviert(btf.btf_sendBuffer19(), btf.e3aktiviert.ma, true)
     btf.sendData(btf.btf_sendBuffer19())
