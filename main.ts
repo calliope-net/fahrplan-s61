9b4d006a0970abf50cc85bf686472bc35595e8d7
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-10-10 21:06:03 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,17 +2,18 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     btf.fill_sendBuffer19()
     btf.setBetriebsart(btf.btf_sendBuffer19(), btf.e0Betriebsart.p2Fahrplan)
     btf.sendData(btf.btf_sendBuffer19())
-    sender.send20Strecken(
+    sender.send2Strecken(
     btf.btf_sendBuffer19(),
-    sender.sender_StreckePicker(100, 90, 50),
-    sender.sender_StreckePicker(-50, 30, 20),
-    sender.sender_StreckePicker(20, 90, 20, true),
+    sender.sender_1MotorPicker(50, 90, 50),
     null,
     null,
-    1
+    null,
+    null,
+    1,
+    true,
+    true,
+    btf.e3Abstand.u1
     )
-    btf.setAbstand(btf.btf_sendBuffer19(), btf.e3Abstand.u2)
-    btf.setaktiviert(btf.btf_sendBuffer19(), btf.e3aktiviert.m1, true)
     btf.sendData(btf.btf_sendBuffer19())
     btf.zeige5x5Buffer(btf.btf_sendBuffer19())
     btf.zeige5x5Joystick(btf.btf_sendBuffer19())
@@ -36,16 +37,19 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     btf.fill_sendBuffer19()
     btf.setBetriebsart(btf.btf_sendBuffer19(), btf.e0Betriebsart.p2Fahrplan)
     btf.sendData(btf.btf_sendBuffer19())
-    sender.send2x2Motoren(
+    sender.send2Strecken(
     btf.btf_sendBuffer19(),
-    sender.sender_2MotorenPicker(80, 80, 25),
-    sender.sender_2MotorenPicker(50, -50, 25),
-    1
+    sender.sender_1MotorPicker(-50, 90, 50),
+    null,
+    null,
+    null,
+    null,
+    1,
+    true,
+    true,
+    btf.e3Abstand.u1
     )
-    btf.setaktiviert(btf.btf_sendBuffer19(), btf.e3aktiviert.ma, true)
     btf.sendData(btf.btf_sendBuffer19())
-    btf.zeige5x5Buffer(btf.btf_sendBuffer19())
-    btf.zeige5x5Joystick(btf.btf_sendBuffer19())
 })
 input.onButtonEvent(Button.B, btf.buttonEventValue(ButtonEvent.Hold), function () {
     btf.buttonBhold()
