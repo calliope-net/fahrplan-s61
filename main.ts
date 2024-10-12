54405dd3f94e8d6d8f8a876a5462ecb0e77cc3a0
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-10-12 11:47:38 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.onButtonEvent(Button.AB, btf.buttonEventValue(ButtonEvent.Hold), function () {
+    btf.fill_sendBuffer19()
+    btf.sendData(btf.btf_sendBuffer19())
+    sender.send2x2Motoren(
+    btf.btf_sendBuffer19(),
+    sender.sender_2MotorenPicker(80, 80, 30),
+    sender.sender_2Motoren(btf.speedPicker(25), btf.speedPicker(-25), 198, 198, true, 1),
+    4,
+    true,
+    true,
+    btf.e3Abstand.u1
+    )
+    btf.sendData(btf.btf_sendBuffer19())
+    btf.zeige5x5Buffer(btf.btf_sendBuffer19())
+    btf.zeige5x5Joystick(btf.btf_sendBuffer19())
+})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     btf.fill_sendBuffer19()
     btf.sendData(btf.btf_sendBuffer19())
@@ -13,24 +29,6 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     true,
     btf.e3Abstand.u0
     )
-    btf.sendData(btf.btf_sendBuffer19())
-    btf.zeige5x5Buffer(btf.btf_sendBuffer19())
-    btf.zeige5x5Joystick(btf.btf_sendBuffer19())
-})
-input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    btf.fill_sendBuffer19()
-    btf.setBetriebsart(btf.btf_sendBuffer19(), btf.e0Betriebsart.p2Fahrplan)
-    btf.sendData(btf.btf_sendBuffer19())
-    sender.send2x2Motoren(
-    btf.btf_sendBuffer19(),
-    sender.sender_2MotorenPicker(80, 80, 25),
-    sender.sender_2Motoren(btf.speedPicker(25), btf.speedPicker(-25), 198, 198, true, 1),
-    4,
-    true,
-    true,
-    btf.e3Abstand.u1
-    )
-    btf.setaktiviert(btf.btf_sendBuffer19(), btf.e3aktiviert.ma, true)
     btf.sendData(btf.btf_sendBuffer19())
     btf.zeige5x5Buffer(btf.btf_sendBuffer19())
     btf.zeige5x5Joystick(btf.btf_sendBuffer19())
@@ -55,9 +53,9 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     btf.zeige5x5Joystick(btf.btf_sendBuffer19())
 })
 input.onButtonEvent(Button.B, btf.buttonEventValue(ButtonEvent.Hold), function () {
-    btf.buttonBhold()
+	
 })
 input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
-    btf.buttonAhold()
+	
 })
-sender.beimStart(true)
+sender.beimStart(true, btf.eFunkgruppe.b1)
